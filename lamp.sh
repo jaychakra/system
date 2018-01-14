c9b5d3cc6a65f21cec274c6116e82690b9a34420
Jay Chakra <jaychakra.jc@gmail.com>
2018-01-14 23:49:46 +0530

    Updated lamp for more php plugins

--- lamp.sh
+++ lamp.sh
@@ -6,5 +6,12 @@ add-apt-repository ppa:ondrej/php
 echo "Updating Cache"
 apt-get update
 
-echo "Installing LAMP with PHPMyadmin"
-apt-get -y install apache2 php7.1 mysql-server phpmyadmin  php7.1-curl php7.1-json php7.1-mysql php7.1-mbstring
+echo "Installing LAMP with PHPMyadmin" 
+apt-get -y install apache2 php7.1 mysql-server phpmyadmin  php7.1-curl php7.1-json php7.1-mysql php7.1-mbstring php7.1-mcrypt
+
+echo "Enabling mod-rewrite"
+a2enmod rewrite
+
+echo "Reloading Apache2"
+systemctl restart apache2
+
